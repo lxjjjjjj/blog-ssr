7b7907ad549120c82b75c01ee45ef59bbb3769de
sarahliu <sarahliu@didiglobal.com>
2021-11-15 20:22:47 +0800

    修改

--- client/components/Author.js
+++ client/components/Author.js
@@ -7,7 +7,7 @@ const Author =()=>{
         <div className={styles["author-div"]}>
             <div> <Avatar size={100} src={'https://wx3.sinaimg.cn/mw690/006Ul03Sly1gbbhm4knc0j31o0280e81.jpg'} /></div>
             <div className={styles["author-introduction"]}>
-               技术和才艺于一身的程序媛，专注于WEB和移动前端开发。
+               还有持续集成部署没做呀
                 <Divider >社交账号</Divider>
                 <Avatar size={28} icon="github" className={styles["account"]}  />
                 <Avatar size={28} icon="qq"  className={styles["account"]} />
